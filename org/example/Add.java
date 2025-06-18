55ff5bc9a3627aac8e680af057c659c20914dbfe
saicharan <saicharan.epparapally@gmail.com>
2025-06-18 16:14:06 +0530

    added

---
+++ org/example/Add.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Add {
+    static int a = 9;
+    static int b = 4;
+
+    public static void main(String[] args) {
+        System.out.println(a+b);
+    }
+
+}
---
+++ org/example/Multiply.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Multiply {
+    static int a = 9;
+    static int b = 4;
+    static int c = 5;
+
+    public static void main(String[] args) {
+        System.out.println(a*b);
+    }
+}
